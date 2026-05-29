4bea15499e34e145c21a696f30bf73761957de86
Brogolem35 <brogolem35@protonmail.com>
2026-05-29 13:19:36 +0300

    Remove redundant lifetime annotation

--- src/icons.rs
+++ src/icons.rs
@@ -1,6 +1,6 @@
-pub static PLAY: &'static [u8] = include_bytes!("../icons/play.svg");
-pub static PAUSE: &'static [u8] = include_bytes!("../icons/pause.svg");
-pub static PREV: &'static [u8] = include_bytes!("../icons/prev.svg");
-pub static NEXT: &'static [u8] = include_bytes!("../icons/next.svg");
-pub static FAVORITE: &'static [u8] = include_bytes!("../icons/favorite.svg");
-pub static FAVORITED: &'static [u8] = include_bytes!("../icons/favorited.svg");
+pub static PLAY: &[u8] = include_bytes!("../icons/play.svg");
+pub static PAUSE: &[u8] = include_bytes!("../icons/pause.svg");
+pub static PREV: &[u8] = include_bytes!("../icons/prev.svg");
+pub static NEXT: &[u8] = include_bytes!("../icons/next.svg");
+pub static FAVORITE: &[u8] = include_bytes!("../icons/favorite.svg");
+pub static FAVORITED: &[u8] = include_bytes!("../icons/favorited.svg");
